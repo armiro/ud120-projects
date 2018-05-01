3ad2bf0b7f6dacd6bb968b0239b45c5d0e2450bc
Udars <arman.hgh.1374@gmail.com>
2018-05-01 12:58:39 +0430

    'solutionForDT'

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print ('the training time is: ', round(time()-t0, 3))
+t1 = time()
+predictions = clf.predict(features_test)
+print ('the test time is: ', round(time()-t1, 3))
+from sklearn.metrics import accuracy_score
+theAcc = accuracy_score(predictions, labels_test)
+print ('the accuracy is: ', round(theAcc, 3))
+print ('number of features is: ', len(features_train[0]))
 #########################################################
 
 
