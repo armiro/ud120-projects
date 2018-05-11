cb5db86f88a1c10387a3ebdf7af22a5772c1f79a
Udars <arman.hgh.1374@gmail.com>
2018-05-11 19:03:26 +0430

    visualizing

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,19 +1,30 @@
-#!/usr/bin/python
+#!/C:/Python27
 
+import matplotlib.pyplot as plt
 import pickle
 import sys
-import matplotlib.pyplot
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+# removing spreadsheet quirk for including TOTAL as a data sample
+data_dict.pop('TOTAL')
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    plt.scatter(salary, bonus)
+plt.xlabel('salary')
+plt.ylabel('bonus')
+plt.show()
 
 
 
